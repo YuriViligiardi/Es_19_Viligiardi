5a95a45895c2e505385771f58984b5e940e77087
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-10 09:44:57 +0200

    Aggiunto array e funzione per la creazione dei Radio Button

--- Es_RadioButton.php
+++ Es_RadioButton.php
@@ -7,6 +7,19 @@
 </head>
 <body>
     <?php
+    $domanda = "Qual è il pianeta più vicino al sole?";
+    $risposte = ["Venere", "Terra", "Marte", "Mercurio", "Giove"];
+    $nameRadioB = "Risposta";
+    echo "<p><b><i>$domanda</i></b></p>";
+    createRadioButton($risposte, $nameRadioB);
+
+        function createRadioButton($array, $name){
+            foreach ($array as $elem) {
+                echo "<input type='radio' name='$name'>";
+                echo "<label for='$elem'>$elem</label>";
+                echo "<br>";
+            }
+        }
     ?>
 </body>
 </html>
